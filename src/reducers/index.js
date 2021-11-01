ebb892df3c445e4060c78a3c4a3bb0de2ea9e768
David Fletcher <davidfletcher@Davids-Air-30.lan>
2021-11-01 12:49:02 -0700

    the 1 is connected

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,14 @@
 import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
 
 export const initialState = {
-    total: 100,
-    operation: "*",
-    memory: 100
+    total: 0,
+    operation: "+",
+    memory: 0
 }
 
 const calculateResult = (num1, num2, operation) => {
     switch(operation) {
-        case("+"):
+        case(ADD_ONE):
             return num1 + num2;
         case("*"):
             return num1 * num2;
